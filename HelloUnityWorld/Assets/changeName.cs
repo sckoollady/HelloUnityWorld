073a77dfe0d263021d3b6d11b5e685a40a5f8e7d
Selena <se12na@knights.ucf.edu>
2019-07-06 19:25:43 -0400

    Added my name after user presses space

---
+++ HelloUnityWorld/Assets/changeName.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class changeName : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     TextMeshProUGUI myText = GetComponent<TextMeshProUGUI>();
+     if (Input.GetKeyDown("space")){
+         myText.SetText("Hello World from Selena Coffman");
+         Debug.Log("Key Pressed");
+     }
+     if (Input.GetKeyDown(KeyCode.Escape)){
+         Application.Quit();
+     }
+    
+    }
+}
